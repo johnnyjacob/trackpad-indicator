4a1fc6d1d7c0b672dd90fc28e7f4baae8f876882
Johnny Jacob <johnnyjacob@gmail.com>
2012-11-24 00:11:57 +0530

    Placeholder for xdg icon installer.

---
+++ xdg-install.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+sudo xdg-icon-resource install --novendor --size 16 touchpad-icon-disabled.png touchpad-disabled
+sudo xdg-icon-resource install --novendor --size 16 touchpad-icon-active.png touchpad-active
